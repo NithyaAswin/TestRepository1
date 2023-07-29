6c81418e1851e1aadc30af308e0566ea266d9992
Admin <Admin@DESKTOP-J5DFPFK>
2023-07-29 21:02:01 +0530

    commit1

---
+++ src/main/java/SuperPackageAssignment/SuperChildAssign.java
@@ -0,0 +1,23 @@
+package SuperPackageAssignment;
+
+
+public class SuperChildAssign extends SuperParentAssign{
+
+	public void display()
+		{
+			System.out.println("super child class");
+			System.out.println(super.a);
+			super.print();//method
+		}
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+
+				//System.out.println(super.s);//can refer only instance variable
+				//cannot call static varaiable
+				SuperChildAssign obj1 = new SuperChildAssign();
+	            obj1.display();
+		//obj1.print();
+			}
+
+
+}
---
+++ src/main/java/SuperPackageAssignment/SuperParentAssign.java
@@ -0,0 +1,10 @@
+package SuperPackageAssignment;
+
+public class SuperParentAssign {
+	int a=10;
+	static String s = "check static variable";//static variable
+	public void print()
+	{
+		System.out.println("This is Super parent variable and method");
+	}
+}
